f04aeb255a23e33f1b1d12437c04fb8232cede3d
Scott Penrose <scottp@dd.com.au>
2014-03-26 16:33:56 +1100

    Add lots more of the SBP entries

--- schema/AU1.3/example.sql
+++ schema/AU1.3/example.sql
@@ -10,26 +10,109 @@ CREATE TABLE IF NOT EXISTS SchoolInfo (
 	RefId varchar(36) UNIQUE,
 	LocalId varchar(200),
 	SchoolName varchar(2000)
+	StateProvinceId varchar(200),
+	CommonwealthId varchar(200),
+	-- Campus (and associated elements)
+	-- Campus/SchoolCampusId
+	-- Campus/AdminStatus 
+	-- Campus/CampusType
+	SchoolSector varchar(200),
+	OperationalStatus varchar(200),
+	IndependentSchool varchar(200),
+	SchoolType varchar(200),
+	Address_StateProvince varchar(200),
+	Address_City varchar(200),
+	Address_PostalCode varchar(200),
+	Address_Street_StreetNumber varchar(200),
+	Address_Street_StreetName varchar(200),
+	Address_GridLocation_Longitude varchar(200),
+	Address_GridLocation_Latitude varchar(200),
+	Address_SchoolGeographicLocation varchar(200),
+	Address_ARIA varchar(200),
+	Entity_Open varchar(200),
+	Entity_Close varchar(200)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--TODO: Multiple entries
+-- SchoolFocusList
+-- SchoolFocusList/SchoolFocus
+-- SchoolGroupList
+-- SchoolGroupList/SchoolGroup
+-- AddressList
+-- AddressList/Address
+-- OtherIdList
+-- OtherIdList/OtherId
 
 CREATE TABLE IF NOT EXISTS StudentPersonal (
 	RefId varchar(36) UNIQUE,
 	LocalId varchar(200),
 	FamilyName varchar(2000),
 	GivenName varchar(2000),
-	SchoolInfo_RefId varchar(36),	-- Why not School Ref ID ?
+	MiddleName varchar(2000),
+	PreferredGivenName varchar(2000),
+	SchoolInfo_RefId varchar(36), -- TODO: Might use StudentSchoolEnrollment
 	YearLevel varchar(2000),
+	StateProvinceId varchar(200),
+	Sex varchar(200),
+	BirthDate varchar(200),
+	IndigenousStatusvarchar(200),
+	CountryofBirthvarchar(200),
+	MostRecent_YearLevel varchar(200),
+	MostRecent_Parent1Language varchar(200),
+	MostRecent_Parent2Language varchar(200),
+	MostRecent_Parent1SchoolEducation varchar(200),
+	MostRecent_Parent2SchoolEducation varchar(200),
+	MostRecent_Parent1NonSchoolEducation varchar(200),
+	MostRecent_Parent2NonSchoolEducation varchar(200),
+	MostRecent_Parent1EmploymentType varchar(200),
+	MostRecent_Parent2EmploymentType varchar(200),
 	FOREIGN KEY (SchoolInfo_RefId) REFERENCES SchoolInfo(RefId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- TODO: Lists
+-- PersonInfo/Demographics/LanguageList
+-- PersonInfo/Demographics/LanguageList/Language
+-- OtherIdList
+-- OtherIdList/OtherId
+-- PersonInfo/AddressList
+-- PersonInfo/AddressList/Address
+-- PersonalInfo/AddressList/Street/StreetNumber
+-- PersonalInfo/AddressList/Street/StreetName
+-- PersonalInfo/AddressList/City
+-- PersonalInfo/AddressList/StateProvince
+-- PersonalInfo/AddressList/PostalCode
+-- PersonalInfo/AddressList/GridLocation/Longitude
+-- PersonalInfo/AddressList/GridLocation/Latitude
+-- PersonInfo/EmailList 
+-- PersonInfo/EmailList/Email 
+-- PersonInfo/EmailList/Email/Type 
+
+-- TODO: StudentSchoolEnrollment
 
 CREATE TABLE IF NOT EXISTS StaffPersonal (
 	RefId varchar(36) UNIQUE,
 	LocalId varchar(200),
 	FamilyName varchar(2000),
 	GivenName varchar(2000),
-	SchoolInfo_RefId varchar(36),
+	MiddleName varchar(200),
+	PreferredGivenName varchar(200),
+	SchoolInfo_RefId varchar(36),	-- Assignment ???
+	StateProvinceId varchar(200),
+	Sex varchar(200),
+	EmploymentStatus varchar(200),
 	FOREIGN KEY (SchoolInfo_RefId) REFERENCES SchoolInfo(RefId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- TODO: Lists
+-- PersonInfo/PhoneNumberList
+-- PersonInfo/PhoneNumberList/PhoneNumber
+-- PersonInfo/PhoneNumberList/PhoneNumber/Number 
+-- PersonInfo/PhoneNumberList/PhoneNumber/Type 
+-- OtherIdList
+-- OtherIdList/OtherId
+-- PersonInfo/EmailList
+-- PersonInfo/EmailList/Email 
+-- PersonInfo/EmailList/Email/Type 
 
 CREATE TABLE IF NOT EXISTS StaffAssignment (
 	RefId varchar(36) UNIQUE,
@@ -38,9 +121,17 @@ CREATE TABLE IF NOT EXISTS StaffAssignment (
 	StaffPersonal_RefId varchar(36),
 	Description varchar(200),
 	PrimaryAssignment varchar(200),
+	JobStartDate varchar(200),
+	JobEndDate varchar(200),
+	JobFunction varchar(200),
+	StaffActivity_Code varchar(200),
 	FOREIGN KEY (SchoolInfo_RefId) REFERENCES SchoolInfo(RefId),
 	FOREIGN KEY (StaffPersonal_RefId) REFERENCES StaffPersonal(RefId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- TODO StudentContactPersonal
+
+-- TODO StudentContactRelationship
 
 CREATE TABLE IF NOT EXISTS RoomInfo (
 	RefId varchar(36) UNIQUE,
@@ -132,3 +223,20 @@ CREATE TABLE IF NOT EXISTS TimeTableCell (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+-- TODO: Consider changing this to be more local styled
+--	e.g. just drop SIF_RefId etc, and link one to one?
+--	More like a users table in LDAP
+CREATE TABLE IF NOT EXISTS Identity (
+	RefId varchar(36) UNIQUE,
+	SIF_RefId varchar(36),
+	SIF_RefObject varchar(50),
+	AuthenticationSource varchar(200),
+	IdentityAssertions_IdentityAssertion varchar(200),
+	SchemaName varchar(200),
+	Algorithm varchar(200),
+	KeyName varchar(200),
+	AuthenticationSourceGlobalUID varchar(200),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- TODO: List (mutlipel passwords? but only one identity - lets just do one?)
+--	PasswordList varchar(200),
+--	PasswordList/Password varchar(200),
