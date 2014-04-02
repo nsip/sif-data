2f6a3c7bd4217eeb97b1b7e00ca98c613eed62ea
Patrick Robotham <patrick.robotham2@gmail.com>
2014-04-03 00:48:46 +1100

    make_students.pl accepts a second command line argument specifying School_RefId

--- examples/make_students.pl
+++ examples/make_students.pl
@@ -61,6 +61,33 @@ my $dbh = DBI->connect(
 	{RaiseError => 1, AutoCommit => 1}
 );
 
+# Check second command line argument
+if(defined $ARGV[1]){
+# Check to see if school exists
+  my $sth = $dbh->prepare("SELECT RefId from SchoolInfo WHERE RefId = \"$ARGV[1]\"");
+  $sth->execute();
+  my $i = 0;
+  while(my $row = $sth->fetchrow_hashref){
+    $i++;
+  }
+  if ($i == 0){
+    print "Provided RefId does not exist";
+    exit(1);
+  }
+  # Insert students into specified school
+  my ($lower,$upper) = split(/\.\./, $ARGV[0]);
+  my $num_students = int(rand($upper - $lower)) + $lower;
+  for(my $i = 0; $i < $num_students; $i++){
+    my $student = create_student();
+    my  $sth0 = $dbh->prepare("INSERT INTO StudentPersonal (RefId, LocalId,
+FamilyName, GivenName, SchoolInfo_RefId, YearLevel) Values(?,?,?,?,?,?)");
+    $sth0->execute($student->{refid}, $student->{address}, 
+		   $student->{firstname},$student->{lastname},
+		   $ARGV[1], $student->{yearlevel});
+  }
+}
+
+
 # Get School Info
 my $sth = $dbh->prepare("SELECT * FROM SchoolInfo");
 $sth->execute();
