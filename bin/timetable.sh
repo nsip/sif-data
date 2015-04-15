e3c6053f74d22e0ad9e81d8911cf63fe6cd35ef3
Scott Penrose <scottp@dd.com.au>
2015-04-15 00:40:52 +0000

    Include home dir

--- bin/timetable.sh
+++ bin/timetable.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 export PERL5LIB=lib
 
+cd /home/scottp/nsip/sif-data
+export HOME=/home/scottp
+
 echo "CREATE DB"
 perl bin/create_sif_data.pl --create-database=$1
 echo "CREATE SCHOOLS, STUDENTS, STAFF, ROOMS, TEACHING GROUPS"
