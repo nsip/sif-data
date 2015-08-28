6a75539d4ee1253e0a83a97eb5709a9407c83ca1
Scott Penrose <scottp@dd.com.au>
2015-08-28 06:04:03 +0000

    Create the grading

--- bin/timetable.sh
+++ bin/timetable.sh
@@ -10,4 +10,6 @@ echo "CREATE SCHOOLS, STUDENTS, STAFF, ROOMS, TEACHING GROUPS"
 perl bin/create_sif_data.pl --database=$1 --create-schools=1 --create-students=50..150 --create-staff=5..20 --create-rooms=3..5 --create-teaching-groups=7..20
 echo "CREATE TIME TABLE"
 perl bin/create_sif_data.pl --database=$1 --create-time-table=first
+echo "CREATE GRADING"
+perl bin/create_sif_data.pl --database=$1 --create-grading
 
