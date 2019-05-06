24886569863f51836b79d8e830c70332b8aefde0
Ben Carter <ben@tearsofaunicorn.com>
2019-05-06 13:31:21 +0800

    AGStatusReport

--- schema/migrations/v2.7.0-v2.8.0.sql
+++ schema/migrations/v2.7.0-v2.8.0.sql
@@ -1,3 +1,12 @@
+/*
+ * Simplify link between StudentPersonal_OtherId and Student
+ */
+ALTER TABLE StudentPersonal_OtherId ADD id INT PRIMARY KEY NOT NULL AUTO_INCREMENT FIRST;
+ALTER TABLE StudentAttendanceTimeList_AttendanceTime_OtherCode ADD id INT PRIMARY KEY NOT NULL AUTO_INCREMENT FIRST;
+
+/*
+ * Changes to StudentSchoolEnrollment
+ */
 ALTER TABLE StudentSchoolEnrollment ADD LocalId VARCHAR(200) NULL;
 ALTER TABLE StudentSchoolEnrollment ADD EntryType VARCHAR(200) NULL;
 ALTER TABLE StudentSchoolEnrollment ADD Homeroom_RefId VARCHAR(36) NULL;
@@ -7,13 +16,13 @@ ALTER TABLE StudentSchoolEnrollment ADD Homegroup VARCHAR(200) NULL;
 ALTER TABLE StudentSchoolEnrollment ADD AcaraSchoolId VARCHAR(200) NULL;
 ALTER TABLE StudentSchoolEnrollment ADD TestLevel VARCHAR(200) NULL;
 ALTER TABLE StudentSchoolEnrollment ADD House VARCHAR(200) NULL;
-ALTER TABLE StudentSchoolEnrollment ADD IndividualLearningPlan VARCHAR(10) NULL;
+ALTER TABLE StudentSchoolEnrollment ADD IndividualLearningPlan VARCHAR(20) NULL;
 ALTER TABLE StudentSchoolEnrollment ADD Calendar_RefId VARCHAR(36) NULL;
-ALTER TABLE StudentSchoolEnrollment ADD ExitStatus VARCHAR(10) NULL;
-ALTER TABLE StudentSchoolEnrollment ADD ExitType VARCHAR(10) NULL;
-ALTER TABLE StudentSchoolEnrollment ADD FTPTStatus VARCHAR(10) NULL;
-ALTER TABLE StudentSchoolEnrollment ADD FFPOS VARCHAR(10) NULL;
-ALTER TABLE StudentSchoolEnrollment ADD CatchmentStatus VARCHAR(10) NULL;
+ALTER TABLE StudentSchoolEnrollment ADD ExitStatus VARCHAR(20) NULL;
+ALTER TABLE StudentSchoolEnrollment ADD ExitType VARCHAR(20) NULL;
+ALTER TABLE StudentSchoolEnrollment ADD FTPTStatus VARCHAR(20) NULL;
+ALTER TABLE StudentSchoolEnrollment ADD FFPOS VARCHAR(20) NULL;
+ALTER TABLE StudentSchoolEnrollment ADD CatchmentStatus VARCHAR(20) NULL;
 ALTER TABLE StudentSchoolEnrollment ADD PreviousSchool VARCHAR(200) NULL;
 ALTER TABLE StudentSchoolEnrollment ADD PreviousSchoolName VARCHAR(200) NULL;
 ALTER TABLE StudentSchoolEnrollment ADD DestinationSchool VARCHAR(200) NULL;
@@ -29,11 +38,6 @@ ALTER TABLE StudentSchoolEnrollment DROP INDEX StudentPersonal_RefId;
 CREATE UNIQUE INDEX StudentSchoolEnrollment_RefId_IX ON StudentSchoolEnrollment (RefId);
 CREATE INDEX StudentSchoolEnrollment_StudentPersonal_RefId_IX ON StudentSchoolEnrollment (StudentPersonal_RefId);
 CREATE INDEX StudentSchoolEnrollment_SchoolInfo_RefId_IX ON StudentSchoolEnrollment (SchoolInfo_RefId);
-ALTER TABLE PersonPicture MODIFY COLUMN RefId varchar(36) NOT NULL;
-ALTER TABLE PersonPicture ADD CONSTRAINT PersonPicture_PK PRIMARY KEY (RefId);
-CREATE UNIQUE INDEX PersonPicture_RefId_IX ON PersonPicture (RefId);
-
-ALTER TABLE StudentPersonal_OtherId ADD id INT PRIMARY KEY NOT NULL AUTO_INCREMENT FIRST;
 
 CREATE TABLE StudentSchoolEnrollment_PublishingPermission (
    ID INT(11) NOT NULL AUTO_INCREMENT,
@@ -71,6 +75,14 @@ CREATE TABLE StudentSchoolEnrollment_StudentSubjectChoice (
    CONSTRAINT SSE_StudentSubjectChoice_FK FOREIGN KEY (StudentSchoolEnrollment_RefId) REFERENCES StudentSchoolEnrollment (RefId)
 ) Engine=InnoDB DEFAULT Charset=utf8;
 
+
+/**
+ * Changes to PersonPicture
+ */
+ALTER TABLE PersonPicture MODIFY COLUMN RefId varchar(36) NOT NULL;
+ALTER TABLE PersonPicture ADD CONSTRAINT PersonPicture_PK PRIMARY KEY (RefId);
+CREATE UNIQUE INDEX PersonPicture_RefId_IX ON PersonPicture (RefId);
+
 CREATE TABLE PersonPicture_PublishingPermission (
    ID INT(11) NOT NULL AUTO_INCREMENT,
    PersonPicture_RefId VARCHAR(36) NOT NULL,
@@ -81,3 +93,95 @@ CREATE TABLE PersonPicture_PublishingPermission (
    INDEX PP_PublishingPermission_IX (PersonPicture_RefId),
    CONSTRAINT PP_PublishingPermission_FK FOREIGN KEY (PersonPicture_RefId) REFERENCES PersonPicture (RefId)
 ) Engine=InnoDB DEFAULT Charset=utf8;
+
+/**
+ * Changes to StaffAssignment
+ */
+ALTER TABLE StaffAssignment ADD JobFTE VARCHAR(20) NULL;
+ALTER TABLE StaffAssignment ADD EmploymentStatus VARCHAR(20) NULL;
+ALTER TABLE StaffAssignment ADD CasualReliefTeacher VARCHAR(20) NULL;
+ALTER TABLE StaffAssignment ADD Homegroup VARCHAR(200) NULL;
+ALTER TABLE StaffAssignment ADD House VARCHAR(200) NULL;
+ALTER TABLE StaffAssignment ADD PreviousSchoolName VARCHAR(200) NULL;
+
+CREATE TABLE StaffAssignment_YearLevel (
+   StaffAssignment_RefId VARCHAR(36) NOT NULL,
+   YearLevel VARCHAR(20) NOT NULL,
+   PRIMARY KEY (StaffAssignment_RefId, YearLevel),
+   INDEX SA_YearLevel_FKX (StaffAssignment_RefId),
+   CONSTRAINT SA_YearLevel_FK FOREIGN KEY (StaffAssignment_RefId) REFERENCES StaffAssignment (RefId)
+) Engine=InnoDB DEFAULT Charset=utf8;
+
+CREATE TABLE StaffAssignment_CalendarSummary (
+   StaffAssignment_RefId VARCHAR(36) NOT NULL,
+   CalendarSummary_RefId VARCHAR(36) NOT NULL,
+   PRIMARY KEY (StaffAssignment_RefId, CalendarSummary_RefId),
+   INDEX SA_CalendarSummary_FKX (StaffAssignment_RefId),
+   CONSTRAINT SA_CalendarSummary_FK FOREIGN KEY (StaffAssignment_RefId) REFERENCES StaffAssignment (RefId)
+) Engine=InnoDB DEFAULT Charset=utf8;
+
+CREATE TABLE StaffAssignment_StaffSubject (
+   ID INT(11) NOT NULL AUTO_INCREMENT,
+   StaffAssignment_RefId VARCHAR(36) NOT NULL,
+   PreferenceNumber VARCHAR(200),
+   SubjectLocalId VARCHAR(200),
+   TimeTableSubject_RefId VARCHAR(36),
+   PRIMARY KEY (id),
+   UNIQUE INDEX SA_StaffSubject_PKX (ID),
+   INDEX SA_StaffSubject_IX (StaffAssignment_RefId),
+   CONSTRAINT SA_StaffSubject_FK FOREIGN KEY (StaffAssignment_RefId) REFERENCES StaffAssignment (RefId)  
+) Engine=InnoDB DEFAULT Charset=utf8;
+
+/** 
+ * Changes to StudentPersonal
+ */
+ALTER TABLE StudentPersonal ADD ESLSupport VARCHAR(20) NULL;
+
+/**
+ * Changes for FQ Reporting
+ */
+DROP TABLE FQReporting_FQRule;
+DROP TABLE FQReporting_FQItem;
+DROP TABLE FQReporting_ContextualQuestion;
+DROP TABLE FQReporting_EntityContact_Address_StatisticalArea;
+DROP TABLE FQReporting_EntityContact_Address;
+DROP TABLE FQReporting_EntityContact_Name;
+DROP TABLE FQReporting_EntityContact;
+DROP TABLE FQReporting;
+
+CREATE TABLE AGStatusReport (
+   RefId VARCHAR(36) NOT NULL PRIMARY KEY,
+   ReportingAuthority VARCHAR(200),
+   ReportingAuthoritySystem VARCHAR(200),
+   ReportingAuthorityCommonwealthId VARCHAR(200),
+   SubmittedBy VARCHAR(200),
+   SubmissionTimestamp VARCHAR(200),
+   AGCollection VARCHAR(200),
+   CollectionYear VARCHAR(200)
+); 
+
+CREATE TABLE AGStatusReport_ReportingObjectResponse (
+   Id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   AGStatusReport_RefId VARCHAR(36) NOT NULL,
+   SubmittedRefId VARCHAR(36),
+   SIFRefId VARCHAR(36),
+   HTTPStatusCode VARCHAR(200),
+   ErrorText VARCHAR(200),
+   CommonwealthId VARCHAR(200),
+   EntityName VARCHAR(200),
+   AGSubmissionStatusCode VARCHAR(200),
+   INDEX `ReportingObjectResponse_AGStatusReport_IX` (`AGStatusReport_RefId`),
+   CONSTRAINT `ReportingObjectResponse_AGStatusReport_FK` FOREIGN KEY (`AGStatusReport_RefId`) REFERENCES `AGStatusReport` (`RefId`)
+);
+
+CREATE TABLE AGStatusReport_AGRule (
+   Id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   ReportingObjectResponse_Id MEDIUMINT NOT NULL,
+   AGRuleCode VARCHAR(200),
+   AGRuleComment VARCHAR(200),
+   AGRuleResponse VARCHAR(200),
+   AGRuleStatus VARCHAR(200),
+   INDEX `Rule_ReportingObjectResponse_IX` (`ReportingObjectResponse_Id`),
+   CONSTRAINT `Rule_ReportingObjectResponse_FK` FOREIGN KEY (`ReportingObjectResponse_Id`) REFERENCES `AGStatusReport_ReportingObjectResponse` (`Id`)
+);
+
