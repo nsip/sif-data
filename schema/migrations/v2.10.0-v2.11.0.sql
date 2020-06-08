42aae6a2393be390cd2a3ff4c5edff9ceb6db015
Ben Carter <ben@tearsofaunicorn.com>
2020-06-08 11:38:00 +0800

    SchoolCourseInfo : New Object

--- schema/migrations/v2.10.0-v2.11.0.sql
+++ schema/migrations/v2.10.0-v2.11.0.sql
@@ -14,3 +14,40 @@ create table TermInfo (
   SchedulingTerm VARCHAR(200) DEFAULT NULL,
   AttendanceTerm VARCHAR(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* SchoolCourseInfo */
+create table SchoolCourseInfo (
+  RefId VARCHAR(36) PRIMARY KEY,
+  SchoolInfo_RefId VARCHAR(36) DEFAULT NULL,
+  SchoolLocalId VARCHAR(200) DEFAULT NULL,
+  SchoolYear VARCHAR(200) DEFAULT NULL,
+  TermInfo_RefId VARCHAR(36) DEFAULT NULL,
+  CourseCode VARCHAR(200) DEFAULT NULL,
+  StateCourseCode VARCHAR(200) DEFAULT NULL,
+  DistrictCourseCode VARCHAR(200) DEFAULT NULL,
+  CourseTitle VARCHAR(200) DEFAULT NULL,
+  Description VARCHAR(2000) DEFAULT NULL,
+  InstructionalLevel VARCHAR(200) DEFAULT NULL,
+  CourseCredits VARCHAR(200) DEFAULT NULL,
+  CoreAcademicCourse VARCHAR(200) DEFAULT NULL,
+  GraduationRequirement VARCHAR(200) DEFAULT NULL,
+  Department VARCHAR(200) DEFAULT NULL,
+  CourseContent VARCHAR(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table SchoolCourseInfo_SubjectArea (
+  Id MEDIUMINT AUTO_INCREMENT PRIMARY KEY,
+  SchoolCourseInfo_RefId VARCHAR(36) NOT NULL,
+  Code VARCHAR(200) DEFAULT NULL
+  INDEX `SchoolCourseInfo_IX` (`SchoolCourseInfo_RefId`),
+  CONSTRAINT `SchoolCourseInfo_FK` FOREIGN KEY (`SchoolCourseInfo_RefId`) REFERENCES `SchoolCourseInfo` (`RefId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table SchoolCourseInfo_SubjectArea_Code (
+  Id MEDIUMINT AUTO_INCREMENT PRIMARY KEY,
+  SubjectArea_Id MEDIUMINT NOT NULL,
+  Value VARCHAR(200) DEFAULT NULL,
+  Codeset VARCHAR(200) DEFAULT NULL,
+  INDEX `SubjectArea_IX` (`SubjectArea_Id`),
+  CONSTRAINT `SubjectArea_FK` FOREIGN KEY (`SubjectArea_Id`) REFERENCES `SchoolCourseInfo_SubjectArea` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
