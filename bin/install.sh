83f7ea9178173b776735633169b00cd44af4df8a
NSIP Update <info@nsip.edu.au>
2022-04-05 05:20:48 +0000

    Installer

---
+++ bin/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo apt install mysql-client libdbd-mysql-perl libjson-perl libtext-csv-perl libdata-uuid-perl libfile-sharedir-perl libfile-sharedir-install-perl
+sudo cpanm -i Data::RandomPerson 
